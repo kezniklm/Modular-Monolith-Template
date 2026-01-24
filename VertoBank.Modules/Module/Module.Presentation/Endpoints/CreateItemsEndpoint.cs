c6c5584afff5267f3feaade80883adf2bc47b197
Keznikl, Matej <matej.keznikl@gmail.com>
2026-01-24 19:30:24 +0100

    feat: add example endpoint

---
+++ VertoBank.Modules/Module/Module.Presentation/Endpoints/CreateItemsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Wolverine.Http;
+
+namespace Module.Presentation.Endpoints;
+
+public static class CreateItemsEndpoint
+{
+    [WolverinePost("/items")]
+    [Tags("module")]
+    [EndpointSummary("Create a new item")]
+    [EndpointDescription("Creates a new item with the specified name and price")]
+    public static Created<ItemCreatedResponse> Create(CreateItemRequest request)
+    {
+        var newId = 1;
+        var response = new ItemCreatedResponse(newId, request.Name, request.Price);
+
+        return TypedResults.Created($"/items/{newId}", response);
+    }
+
+    public record CreateItemRequest(string Name, decimal Price);
+
+    public record ItemCreatedResponse(int Id, string Name, decimal Price);
+}
