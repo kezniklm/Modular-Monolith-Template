ca842e647f77618ba0d96c2a3b4a6ea5e388ea20
Keznikl, Matej <matej.keznikl@gmail.com>
2026-01-29 08:44:43 +0100

    feat: add common errors

---
+++ VertoBank.Modules/Module/Module.Domain/Common/Errors.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+
+namespace Module.Domain.Common;
+
+public abstract class ValidationError(string message) : Error(message);
+
+public abstract class DomainError(string message) : Error(message);
+
+public abstract class NotFoundError(string message) : Error(message);
+
+public abstract class ForbiddenError(string message) : Error(message);
